4990ecea083cbfa7eec3e139e3eaeda524c584bf
Levi Sanches <levi_sanches@outlook.com>
2023-07-13 10:37:53 -0300

    roteamento, lazyloading e guard

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { authGuard } from './auth.guard';
+
+const routes: Routes = [
+  { path: 'primeira-pagina', component: PrimeiraPaginaComponent},
+  { path: 'segunda-pagina', component: SegundaPaginaComponent},
+  { path: "", redirectTo: 'primeira-pagina', pathMatch: 'full'},
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
+  { path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate: [authGuard]},
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "**", component: PaginaNaoEncontradaComponent},
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
